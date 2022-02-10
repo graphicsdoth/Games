83429e6e01fd3f7f4d04663f42276b2eac423397
Anisha Joshi <78501533+graphicsdoth@users.noreply.github.com>
2022-02-09 18:28:07 -0600

    Hirst Painting using dots in Python
    https://www.artsy.net/artist-series/damien-hirst-spots. 
    The spot painting is worth thousands of dollars. I tried creating it using the Turtle Graphics - https://docs.python.org/3/library/turtle.html
    
    I started  by  downloading  an  image of the painting and  then  choosing all the  colors in that using extract

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
-from data import question_data
-from question_model import Question
-from quiz_brain import QuizBrain
+import colorgram
+import turtle
+from turtle import Turtle, Screen
+import random
 
-question_bank = []
-l= len(question_data)
-##creating a list of all questions
-for i in question_data:
-    q_obj = Question(i["question"], i["correct_answer"])
-    question_bank.append(q_obj)
+# Extract 40 colors from an image.
+# colors = colorgram.extract('artimage.jpg', 40)
 
- ## creating an object for class QuizBrain
-ques = QuizBrain(question_bank)
+# # colorgram.extract returns Color objects, which let you access
+# # RGB, HSL, and what proportion of the image was that color.
+# first_color = colors[0]
+# rgb = first_color.rgb # e.g. (255, 151, 210)
+# hsl = first_color.hsl # e.g. (230, 255, 203)
+# proportion  = first_color.proportion # e.g. 0.34
+# list_col =[]
+# for color in colors:
+#     list_col.append((color.rgb.r,color.rgb.g,color.rgb.b))
+# print(list_col)
 
-ques.next_question()
-while(ques.still_has_ques() == True):
-    ques.next_question()
+## Saved the extracted image colors in a list
+color_list = [(232, 251, 242), (198, 12, 32), (250, 237, 17), (39, 76, 189), (38, 217, 68), (238, 227, 5), (229, 159, 46), (27, 40, 157), (215, 74, 12), (15, 154, 16), (199, 14, 10), (242, 246, 252), (243, 33, 165), (229, 17, 121), (73, 9, 31), (60, 14, 8), (224, 141, 211), (10, 97, 61), (221, 160, 9), (17, 18, 43), (46, 214, 232), (11, 227, 239), (81, 73, 214), (238, 156, 220), (74, 213, 167), (77, 234, 202), (52, 234, 243), (3, 67, 40), (218, 87, 49), (174, 178, 231), (237, 169, 164), (6, 245, 223), (247, 9, 42), (10, 79, 112), (16, 54, 243), (240, 16, 16)]
 
-print("\n")
-print("You have completed the quiz")
-print(f"Your Final Score is {ques.score}/{ques.quest_num}")
+turtle.colormode(255)
+def random_color():
+    tup = color_list[random.randint(0,l)]
+    return(tup)
 
 
 
+tim = Turtle()
+l= len(color_list)
+print(tim.xcor())
+print(tim.ycor())
+tim.hideturtle()
+tim.penup()
+tim.forward(-300)
+tim.right(90)
+tim.forward(300)
+tim.left(90)
+for j in range(10):
+    for i in range(10):
+        tim.pendown()
+        tup = color_list[random.randint(0,l-1)]
+        # print(tup)
+        tim.pencolor(tup)
+        # tim.showturtle()
+        tim.speed(6)
+        tim.dot(20)
+        tim.penup()
+        tim.fd(50)
+    # tim.hideturtle()
+    tim.speed(0)
+    tim.forward(-500)
+    tim.left(90)
+    tim.fd(50)
+    tim.right(90)
+
+
+screen= Screen()
+screen.exitonclick()
